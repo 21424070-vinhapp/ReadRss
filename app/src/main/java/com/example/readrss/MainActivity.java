49e78c390c125cdb6505a67fc651d9494c750118
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-25 16:28:44 +0700

    update

--- app/src/main/java/com/example/readrss/MainActivity.java
+++ app/src/main/java/com/example/readrss/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.readrss;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new ReadRSS().execute("https://vnexpress.net/rss/oto-xe-may.rss");
+    }
+
+    private class ReadRSS extends AsyncTask<String,Void,String>
+    {
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder content=new StringBuilder();
+            try {
+
+                URL url=new URL(strings[0]);
+
+                InputStreamReader inputStreamReader=new InputStreamReader(url.openConnection().getInputStream());
+
+                BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+
+                String line="";
+                while((line=bufferedReader.readLine())!=null)
+                {
+                    content.append(line+"\n");
+                }
+
+                bufferedReader.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return content.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+        }
     }
 }
